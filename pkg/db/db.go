9425308a928664b3edcb9a7f3643e84dae5543dc
Sergey <xxsmotur@gmail.com>
2019-05-26 18:02:40 +0500

    init for db package

---
+++ pkg/db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// DB provides handling of db data
+type DB struct {
+	Username string
+	Password string
+	Database string
+	Driver   string
+	Port     int
+	Host     string
+}
+
+// CreateTable provides creating of the migr table
+func CreateTable(d *DB) error {
+	if d == nil {
+		return errors.New("db is not defined")
+	}
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		d.Host, d.Username, d.Password, d.Port, d.Database)
+
+	db, err := sql.Open(d.Driver, connString)
+	if err != nil {
+		return errors.Wrap(err, "error creating connection pool")
+	}
+	ctx := context.Background()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return errors.Wrap(err, "error to ping db")
+	}
+	return nil
+}
