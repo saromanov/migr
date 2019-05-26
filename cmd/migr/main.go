af63e8c573ce10de0a2279b87f251c6793bc9347
Sergey <xxsmotur@gmail.com>
2019-05-26 17:20:26 +0500

    updating cmd line for app

--- cmd/migr/main.go
+++ cmd/migr/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"github.com/pkg/errors"
+	"github.com/urfave/cli"
 	"log"
 	"os"
-
-	"github.com/urfave/cli"
 )
 
+func makeApp(c *cli.Context) error {
+	driver := c.String("driver")
+	if driver == "" {
+		return errors.New("driver is not defined")
+	}
+	return nil
+}
 func main() {
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
@@ -15,11 +22,18 @@ func main() {
 			Value: "",
 			Usage: "SQL driver",
 		},
+		cli.StringFlag{
+			Name:  "new",
+			Value: "",
+			Usage: "Create a new migration",
+		},
 	}
 	app.Name = "migr"
 	app.Usage = "tool for SQL migrations"
 	app.Action = func(c *cli.Context) error {
-
+		if err := makeApp(c); err != nil {
+			log.Fatal(err)
+		}
 		return nil
 	}
 
