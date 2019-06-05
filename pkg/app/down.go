334d7bf2a20ac2cdb86a6d40e378dab46f40364a
Sergey <xxsmotur@gmail.com>
2019-06-05 07:41:32 +0500

    implementation of the down command

--- pkg/app/down.go
+++ pkg/app/down.go
@@ -1,6 +1,18 @@
 package app
 
+import (
+	"github.com/pkg/errors"
+)
+
 // Down defines downgrade for migrations
-func (a *App) Down(path string) error {
+func (a *App) Down(path, version string) error {
+	dirs, err := getMigrDirs(path)
+	if err != nil {
+		return err
+	}
+	if len(dirs) == 0 {
+		return errors.New("migr directories is not found")
+	}
+
 	return nil
 }
