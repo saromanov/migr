491c96bd9997d149531d9a740f289c1b56b6558d
Sergey <xxsmotur@gmail.com>
2019-06-12 21:05:01 +0500

    Add method for show list of migrations

---
+++ pkg/app/info.go
@@ -0,0 +1,18 @@
+package app
+
+import (
+	"github.com/pkg/errors"
+)
+
+// Info returns information about migrations
+func (a *App) Info() error {
+	migs, err := a.db.GetMigrationVersions()
+	if err != nil {
+		return errors.Wrap(err, "unable to get list of migrations")
+	}
+
+	for _, mig := range migs {
+		Info("migration: %d", mig.Version)
+	}
+	return nil
+}
