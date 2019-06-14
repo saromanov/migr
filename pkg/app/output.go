1f1fcda08d682ebab2433616f77d3c62d59fa5c9
Sergey <xxsmotur@gmail.com>
2019-06-15 00:19:43 +0500

    Fixing of printing messages

--- pkg/app/output.go
+++ pkg/app/output.go
@@ -4,10 +4,10 @@ import "github.com/fatih/color"
 
 // Info returns info message during app execution
 func Info(format string, a ...interface{}) {
-	color.Blue(format, a)
+	color.Blue(format, a...)
 }
 
 // Error returns error mesage during app execution
 func Error(format string, a ...interface{}) {
-	color.Red(format, a)
+	color.Red(format, a...)
 }
