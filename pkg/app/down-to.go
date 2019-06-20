8b8fdf1ce972d2c209d75d5810d0e362f680060a
Sergey <xxsmotur@gmail.com>
2019-06-20 07:25:15 +0500

    Implementation of down to

--- pkg/app/down-to.go
+++ pkg/app/down-to.go
@@ -1,6 +1,19 @@
 package app
 
+import "fmt"
+
 // DownTo defines downgrade of migration to specific version
 func (a *App) DownTo(version string) error {
+	dirs, err := getMigrationsDirs(".")
+	if err != nil {
+		return err
+	}
+	for _, d := range dirs {
+		if fmt.Sprintf("%d", d.timestamp) == version {
+			break
+		}
+
+	}
+
 	return nil
 }
