80f179598633018ffa75d6fa01f20336c9c84b48
Sergey <xxsmotur@gmail.com>
2019-06-22 00:30:13 +0500

    Parsing of migration version

--- pkg/app/down-to.go
+++ pkg/app/down-to.go
@@ -2,18 +2,27 @@ package app
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/pkg/errors"
 )
 
 // DownTo defines downgrade of migration to specific version
 func (a *App) DownTo(version string) error {
+	numVersion, err := strconv.ParseInt(version, 10, 64)
+	if err != nil {
+		return errors.Wrap(err, "unable to parse version")
+	}
+	mig, err := a.db.GetMigrationByTheVersion(numVersion)
+	if err != nil {
+		return errors.Wrap(err, "unable to find migration")
+	}
 	dirs, err := getMigrationsDirs(".")
 	if err != nil {
 		return err
 	}
 	for _, d := range dirs {
-		if fmt.Sprintf("%d", d.timestamp) == version {
+		if d.ID == mig.ID {
 			break
 		}
 		if err := a.downgradeMigration(".", d.timestamp); err != nil {
