c5821d65a3c37827ab1af725990096fd328735f7
Sergey <xxsmotur@gmail.com>
2019-06-26 21:20:43 +0500

    init for tests

--- pkg/app/run_test.go
+++ pkg/app/run_test.go
@@ -1,2 +1,16 @@
 package app
 
+import (
+	"testing"
+)
+
+
+const (
+	basicPath = "../testdata"
+)
+func TestRun(t *testing.T) {
+	err := Run(basicPath)
+	if err != nil {
+		t.Errorf("unable to execute run command")
+	}
+}
