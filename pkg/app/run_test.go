4c43a4ed3ea8558b0c0ecaab71f0127706f66a35
Sergey <xxsmotur@gmail.com>
2019-06-27 22:33:18 +0500

    Add init method for tests

--- pkg/app/run_test.go
+++ pkg/app/run_test.go
@@ -1,6 +1,7 @@
 package app_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/saromanov/migr/pkg/app"
@@ -10,7 +11,12 @@ const (
 	basicPath = "../../testdata/basic"
 )
 
-var appTest = app.New("postgres", "pinger", "pinger", "pinger", "pinger", 5432)
+var appTest *app.App
+
+func init() {
+	os.Setenv("MIGR_PATH", basicPath)
+	appTest = app.New("postgres", "pinger", "pinger", "pinger", "pinger", 5432)
+}
 
 func TestRun(t *testing.T) {
 	err := appTest.Run(basicPath)
