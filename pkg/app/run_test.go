d9e73e95d72e8f21caa4ffc26d9e7c2179544826
Sergey <xxsmotur@gmail.com>
2019-06-27 22:21:11 +0500

    Init for tests

--- pkg/app/run_test.go
+++ pkg/app/run_test.go
@@ -1,16 +1,20 @@
-package app
+package app_test
 
 import (
 	"testing"
-)
 
+	"github.com/saromanov/migr/pkg/app"
+)
 
 const (
-	basicPath = "../testdata"
+	basicPath = "../..testdata/basic/"
 )
+
+var appTest = app.New("postgres", "pinger", "pinger", "pinger", "pinger", 5432)
+
 func TestRun(t *testing.T) {
-	err := Run(basicPath)
+	err := appTest.Run(basicPath)
 	if err != nil {
-		t.Errorf("unable to execute run command")
+		t.Errorf("unable to execute run command: %v", err)
 	}
 }
