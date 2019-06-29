dcaa1e3e0c8675727e32cba04e3790a210aeaca7
Sergey <xxsmotur@gmail.com>
2019-06-30 01:07:25 +0500

    Add helpful methods for tests

--- pkg/app/run_test.go
+++ pkg/app/run_test.go
@@ -1,14 +1,19 @@
 package app_test
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
 	"os"
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/saromanov/migr/pkg/app"
 )
 
 const (
-	basicPath = "../../testdata/basic"
+	basicPath       = "../../testdata/basic"
+	migrCreateTable = "CREATE TABLE IF NOT EXISTS migr( id SERIAL, version int8 UNIQUE, changes varchar(128), hash varchar(128), applied bool, error_message varchar(128), failed bool, status varchar(16), created_at int8 )"
 )
 
 var appTest *app.App
@@ -16,6 +21,40 @@ var appTest *app.App
 func init() {
 	os.Setenv("MIGR_PATH", basicPath)
 	appTest = app.New("postgres", "pinger", "pinger", "pinger", "pinger", 5432)
+}
+
+// Create table for tests
+func createTestTable() (*sql.DB, error) {
+	db, err := sql.Open("postgres", "user=migr_test password=migr_test dbname=migr_test")
+	if err != nil {
+		return nil, fmt.Errorf("error creating connection pool: %v", err)
+	}
+	defer db.Close()
+	ctx := context.Background()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "error to ping db")
+	}
+
+	return db, nil
+}
+
+func createMigrTable(db *sql.DB) error {
+	_, err := db.Exec(migrCreateTable)
+	if err != nil {
+		return fmt.Errorf("unable to create migr table: %v", err)
+	}
+
+	return nil
+}
+
+func dropMigrTable(db *sql.DB) error {
+	_, err := db.Exec("DROP TABLE migr")
+	if err != nil {
+		return fmt.Errorf("unable to drop migr table: %v", err)
+	}
+
+	return nil
 }
 
 func TestRun(t *testing.T) {
