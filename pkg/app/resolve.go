aa9108151915768fe965990ed57bc2d935585854
Sergey <xxsmotur@gmail.com>
2019-07-19 21:09:53 +0500

    Starting to implement resolve method

--- pkg/app/resolve.go
+++ pkg/app/resolve.go
@@ -2,5 +2,12 @@ package app
 
 // Resolve provides resolving of migrations
 func (a *App) Resolve(path string) error {
+	dirs, err := getMigrationsDirs(path)
+	if err != nil {
+		return err
+	}
+	if len(dirs) == 0 {
+		return nil
+	}
 	return nil
 }
